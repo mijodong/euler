03775d3e5fdafe71aa9f8185046a1f3df387d873
MJ D. Nguyen <mijodong@users.noreply.github.com>
2021-03-05 01:13:42 -0600

    Remove test prints

--- python/problem-10.py
+++ python/problem-10.py
@@ -8,16 +8,13 @@ for _ in range(max_value):
     sieve.append(True)
 
 for i in range(2, int(round(sqrt(max_value)))):
-    # print("i", i)
     if sieve[i]:
         for j in range(i ** 2, max_value, i):
-            # print("j", j)
             sieve[j] = False
 
 total = 0
 for i in range(2, len(sieve)):
     if sieve[i]:
-        # print(i)
         total += i
 
 print(total)
